4bebd22826f2be24a25c401123295fcf8fd9b32e
MatiasDonati <matod87@hotmail.com>
2022-09-02 11:02:40 -0300

    A o B mejorado

--- desafioAB.js
+++ desafioAB.js
@@ -3,28 +3,39 @@
 // Al finalizar el cilco nos dara el resultado de cual de las dos letras tiene mas votos o si hay un empate.
 
 let eleccionUsuario = "";
+let cantidadDeVotos = Number(prompt('Ingresa un numero ! Ese Numero será la cantidad de veces que Votes.. Que viva la democracia!!!'))
 let a = 0;
 let b = 0;
+let votoImpugnado = 0;
 let sumaA;
 let sumaB;
+let sumavotoImpugnado;
+let palabraVoto
 
 
-for (let i = 1; i <= 10; i++) {
-    let eleccionUsuario = prompt('Elegí A o B, tenes 5 votos!').toUpperCase();
+for (let i = 1; i <= cantidadDeVotos; i++) {
+    let eleccionUsuario = prompt('Elegí A o B, tenes ' +cantidadDeVotos + ' votos!').toUpperCase();
     if (eleccionUsuario === 'A'){
         a++;
     } else if (eleccionUsuario === 'B'){
         b++;
     }else{
+        votoImpugnado++
         alert('A o B !!')
     }
     sumaA = a
     sumaB = b
+    sumavotoImpugnado = votoImpugnado
 }
 if (sumaA > sumaB) {
     alert('A ganó!! recibió '+sumaA +' votos!')
 } else if (sumaB > sumaA) {
     alert('B ganó!! recibió '+sumaB +' votos!')
 } else {
-    alert('Se ha dado un Empate.')
+    if (sumavotoImpugnado > 1){
+        palabraVoto = ' votos impugnados'
+    }else {
+        palabraVoto = ' voto impugnado'
+    }
+    alert('Se ha dado un Empate, ambos han obtenido ' + sumaA + ' votos y ha habido '+ sumavotoImpugnado + palabraVoto)
 }
